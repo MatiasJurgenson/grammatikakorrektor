2c3a6bcc1bfc4a39b14307d8e490b90abb33d7cd
PeetriPeer <60267635+PeetriPeer@users.noreply.github.com>
2020-09-26 10:57:51 +0300

    Add files via upload

---
+++ programss/sub-programs/main_sonastik.py
@@ -0,0 +1,11 @@
+#avab faili
+fail = open("lemmad2013.txt", encoding = "UTF-8")
+for rida in fail:
+    sisu = fail.read()
+    sonastik = sisu.splitlines()
+
+fail.close()
+
+def oigekiri(sona):
+    if sona in sonastik:
+        return "true"    
