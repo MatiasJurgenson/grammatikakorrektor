b7e4ad64a41cadc048d840b414c62a2d1ccd542e
PeetriPeer <60267635+PeetriPeer@users.noreply.github.com>
2021-04-18 20:46:35 +0300

    Add files via upload

--- koostisosad/koma_main.py
+++ koostisosad/koma_main.py
@@ -21,31 +21,40 @@ komata_sidesonad = ["ehk", "nii", "ja", "ning", "või"]
 def komaviga(lause):
     komaveakohad = []
     komaõigesti = []
+    sonakoht = []
+    sonanumber = 0
 
     for word in lause:
         komata_sona = word.replace(",", "")
+        # koma ette panemine
         if komata_sona in komaga or komata_sona == "kui" and lause[lause.index(komata_sona)+1] != "ka":
             x = lause.index(word)
             x -= 1
-            komaveakohad.append(lause[x] + " " + lause[x + 1])
-            komaette = (lause[x] + ",")
-            komaõigesti.append(komaette + " " + lause[x + 1 ])
-                
+            if lause[x][-1] != ",":
+                komaveakohad.append(lause[x] + " " + lause[x + 1])
+                komaette = (lause[x] + ",")
+                komaõigesti.append(komaette + " " + lause[x + 1 ])
+                sonakoht.append(sonanumber)
+        # nud/tud        
         elif len(komata_sona) >= 3 and komata_sona[-3] == "t" and komata_sona[-2] == "u" and komata_sona[-1] == "d" or len(komata_sona) >= 3 and komata_sona[-3] == "n" and komata_sona[-2] == "u" and komata_sona[-1] == "d":
             if komata_sona != lause[0] or komata_sona != lause[-1]:
                 x = lause.index(word)
                 komaette = (komata_sona + ",")
                 komaveakohad.append(lause[x] + " " + lause[x + 1])
                 komaõigesti.append(komaette + " " + lause[x + 1])
-                    
-        elif word in komata_sidesonad or word == "kui" and lause[lause.index(word)+1] == "ka" or word.replace(",", "") not in komaga:
+                sonakoht.append(sonanumber)
+        # koma ära võtmine            
+        elif word in komata_sidesonad or word == "kui" and lause[lause.index(word)+1] == "ka" or komata_sona not in komaga:
             x = lause.index(word)
             x -= 1
-            if lause[x][-1] == "," and len(lause[x]) >= 3 and lause[x][-4] != "t" and lause[x][-3] != "u" and lause[x][-2] != "d" or len(lause[x]) >= 3 and lause[x][-4] != "n" and lause[x][-3] != "u" and lause[x][-2] != "d":
+            if lause[x][-1] == "," and len(lause[x]) >= 4 and lause[x][-4] != "t" and lause[x][-3] != "u" and lause[x][-2] != "d" or len(lause[x]) >= 4 and lause[x][-4] != "n" and lause[x][-3] != "u" and lause[x][-2] != "d":
+                
                 asendatav_sona = lause[x]
                 asendatav_sona = asendatav_sona.replace(",", "")
                 komaveakohad.append(lause[x] + " " + lause[x + 1])
                 komaõigesti.append(asendatav_sona + " " + lause[x + 1 ])
+                sonakoht.append(sonanumber)
                 
+        sonanumber += 1       
                         
-    return [komaveakohad, komaõigesti]
+    return [komaveakohad, komaõigesti, sonakoht]
